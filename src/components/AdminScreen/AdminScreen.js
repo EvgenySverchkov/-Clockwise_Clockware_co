68a5899a4cadcc50b25066ca0dd2ed9a008cddfe
EvgenySverchkov <47426436+EvgenySverchkov@users.noreply.github.com>
2020-06-18 13:22:49 +0300

    added ability to choose town for each masters

--- src/components/AdminScreen/AdminScreen.js
+++ src/components/AdminScreen/AdminScreen.js
@@ -3,66 +3,74 @@ import {connect} from "react-redux";
 import {addNewMaster, toggleAddMasterForm, toggleShowMasterList} from "../../store/actions";
 
 import Sidebar from "./Sidebar";
-import MastersList from "./MastersList"
+import MastersList from "./MastersList";
+import Content from "./Content";
+import AddMasterForm from "./AddMasterForm";
 
 function AdminSrcreen(props){
-	function createUniqueId(){
-		if(props.mastersArr.length === 0){
-			return 1;
-		}else{
-			let copyArr = [...props.mastersArr];
-			let sortArr = copyArr.sort((firstItem,secondItem)=>firstItem.id - secondItem.id);
-			return sortArr[sortArr.length-1].id + 1;
-		}
-
-	}
-	function submitHandler(e){
-		e.preventDefault();
-		let masterName = e.target.name.value;
-		let masterRating = e.target.rating.value;
-		if(masterName||masterRating){
-			let infoObj = {
-				name: e.target.name.value,
-				rating: e.target.rating.value,
-				id: createUniqueId()
-			}
-			props.addNewMaster(infoObj);
-			props.toggleAddMasterForm(false);
-			props.toggleShowMasterList(true);
-			alert("You added new master")
-		}else{
-			alert("Please, feeling all gaps")
-		}
-	}
 	function toggleAddMasterForm(){
 			if(props.isAddMasterForm){
-				return (
-				<form onSubmit={submitHandler}>
-					<label htmlFor="rating">Enter reting</label>
-					<input id="rating" type="number" min="0" max="5"></input>
-					<label htmlFor="name">Enter name</label>
-					<input id="name"></input>
-					<input type="submit" value="Add"></input>
-				</form>
-			)
+				return <AddMasterForm handler={submitHandler} townsArr={props.townsArr}/>;
+			}
+			function submitHandler(e){
+				e.preventDefault();
+				let masterName = e.target.name.value;
+				let masterRating = e.target.rating.value;
+				if(masterName||masterRating){
+					let infoObj = {
+						name: e.target.name.value,
+						rating: e.target.rating.value,
+						id: createUniqueId(),
+						towns: selectCheckedTowns(e.target.elements)
+					}
+					props.addNewMaster(infoObj);
+					props.toggleAddMasterForm(false);
+					props.toggleShowMasterList(true);
+					alert("You added new master")
+				}else{
+					alert("Please, feeling all gaps")
+				}
+			}
+			function createUniqueId(){
+				if(props.mastersArr.length === 0){
+					return 1;
+				}else{
+					let copyArr = [...props.mastersArr];
+					let sortArr = copyArr.sort((firstItem,secondItem)=>firstItem.id - secondItem.id);
+					return sortArr[sortArr.length-1].id + 1;
+				}
+			}
+			function selectCheckedTowns(elements){
+				let newArr = Array.from(elements);
+				let towns = [];
+				newArr.forEach((item)=>{
+					if(item.className==='towns'){
+						if(item.checked){
+							towns.push(item.value)
+						}
+					}
+				})
+				return towns;
 			}
 	}
 	function toggleShowMasterList(){
 		if(props.isMasterList){
-			console.log(props.mastersArr)
 			return <MastersList mastersArr = {props.mastersArr} />
 		}
 	}
+
 	return(
 		<div style={style}>
 			<Sidebar />
-			<div className="itemsList">
-				{toggleAddMasterForm()}
-				{toggleShowMasterList()}
-			</div>
-		</div>);
-	}
-
+			<Content toggleAddMasterForm={toggleAddMasterForm}
+							 toggleShowMasterList={toggleShowMasterList}/>
+		</div>
+	);
+}
+const style={
+	display: "grid",
+	gridTemplateColumns: "25% 75%"
+}
 function mapStateToProps(state){
 	return {
 		mastersArr: state.master_reducer.masters,
@@ -71,14 +79,4 @@ function mapStateToProps(state){
 		isMasterList: state.master_reducer.isMasterList
 	}
 }
-const style={
-	display: "grid",
-	gridTemplateColumns: "25% 75%"
-}
 export default connect(mapStateToProps, {addNewMaster, toggleAddMasterForm, toggleShowMasterList})(AdminSrcreen);
-// <label htmlFor="towns">Choose town</label>
-// <select id="towns" multiple>
-// 	{
-// 		props.townsArr.map((item)=><option key={item}>{item}</option>)
-// 	}
-// </select>
--- src/components/AdminScreen/MastersList/MasterItem.js
+++ src/components/AdminScreen/MastersList/MasterItem.js
@@ -2,9 +2,10 @@ import React from 'react';
 
 export default ({masterObj})=>{
   return (
-    <li key={masterObj.id+1}>
+    <li>
       <div>Name: {masterObj.name}</div>
       <div>Rating: {masterObj.rating}</div>
+      <div>Towns: {masterObj.towns.join(",")}</div>
       <div>Id: {masterObj.id}</div>
     </li>
   );
