5b869f0af96eae01e5d69827565057bb2eec7f3c
EvgenySverchkov <47426436+EvgenySverchkov@users.noreply.github.com>
2020-06-18 09:38:13 +0300

    added ability to create new masters

--- src/components/AdminScreen/AdminScreen.js
+++ src/components/AdminScreen/AdminScreen.js
@@ -1,10 +1,55 @@
-import React from 'react';
-import { NavLink } from "react-router-dom";
+import React, {useState} from 'react';
+import {connect} from "react-redux";
+import {addNewMaster, toggleAddMasterForm} from "../../services/actions";
 
-function AdminSrcreen(){
-	return	(
-		<div>Admin Screen</div>
-	);
+import Sidebar from "./Sidebar"
+
+function AdminSrcreen(props){
+	const [state, setState] = useState();
+	function submitHandler(e){
+		e.preventDefault()
+		let infoObj = {
+			name: e.target.name.value,
+			rating: e.target.rating.value,
+		}
+		props.addNewMaster(infoObj);
+		props.toggleAddMasterForm(false);
+		setState(<div>You added master</div>)
+	}
+	function toggleAddMasterForm(){
+			if(props.isAddMasterForm){
+				return (
+				<form onSubmit={submitHandler}>
+					<label htmlFor="rating">Enter reting</label>
+					<input id="rating"></input>
+					<label htmlFor="name">Enter name</label>
+					<input id="name"></input>
+					<input type="submit" value="Add"></input>
+				</form>
+			)
+			}
+	}
+	return(
+		<div>
+			<Sidebar />
+			<div className="itemsList">
+				{toggleAddMasterForm()}
+				{state}
+			</div>
+		</div>);
+	}
+
+function mapStateToProps(state){
+	return {
+		mastersArr: state.master_reducer.masters,
+		isAddMasterForm: state.master_reducer.isAddMaster,
+		townsArr:state.master_reducer.towns
+	}
 }
-
-export default AdminSrcreen;
+export default connect(mapStateToProps, {addNewMaster, toggleAddMasterForm})(AdminSrcreen);
+// <label htmlFor="towns">Choose town</label>
+// <select id="towns" multiple>
+// 	{
+// 		props.townsArr.map((item)=><option key={item}>{item}</option>)
+// 	}
+// </select>
