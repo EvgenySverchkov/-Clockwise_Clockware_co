9c5993259e39c897fa58ef997302d6a3c10751c1
EvgenySverchkov <47426436+EvgenySverchkov@users.noreply.github.com>
2020-06-22 23:22:10 +0300

    added new dependenc

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter} from "react-router-dom";
-import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+
 import App from './App';
 
 ReactDOM.render((
