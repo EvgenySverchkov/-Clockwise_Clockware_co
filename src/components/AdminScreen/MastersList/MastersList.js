80580c18d8db980f838043d9d22f3a19205ff8ca
EvgenySverchkov <47426436+EvgenySverchkov@users.noreply.github.com>
2020-06-22 16:41:59 +0300

    added ability to delete masters from db

--- src/components/AdminScreen/MastersList/MastersList.js
+++ src/components/AdminScreen/MastersList/MastersList.js
@@ -5,7 +5,13 @@ import {deleteMaster} from "../../../store/adminPanel/actions";
 import withDeleteBtn from "../../../hocs/withDeleteBtn";
 
 function MastersList(props){
-  let MasterItemWithDelete = withDeleteBtn(MasterItem, props.deleteMaster);
+  function deleteHandler(masterId){
+    fetch(`https://clockwiseserver.herokuapp.com/delete/${masterId}`, {
+      method: "delete"
+    }).then(data=>data.json())
+    .then(data=>props.deleteMaster(data))
+  }
+  let MasterItemWithDelete = withDeleteBtn(MasterItem, deleteHandler);
   return (
     <ul style={{listStyle: "none"}}>{
       props.mastersArr.map((item)=><MasterItemWithDelete key={item.id+1} masterObj = {item} by={item.id}/>)
