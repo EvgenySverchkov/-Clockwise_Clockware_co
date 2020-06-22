c90f58146744cbb96f8e22f019edcc9fdb3e209f
EvgenySverchkov <47426436+EvgenySverchkov@users.noreply.github.com>
2020-06-22 16:39:46 +0300

    loading masters array from DB

--- src/components/AdminScreen/AdminScreen.js
+++ src/components/AdminScreen/AdminScreen.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Switch, Route} from "react-router-dom";
 import {connect} from "react-redux";
-import {addNewMaster, addNewTown} from "../../store/adminPanel/actions";
+import {addNewMaster, addNewTown, initMasters} from "../../store/adminPanel/actions";
 
 import Sidebar from "./Sidebar";
 import ClientsList from "./ClientsList";
@@ -11,6 +11,11 @@ import AddNewTownForm from "./AddNewTownForm";
 import AddMasterForm from "./AddMasterForm";
 
 function AdminSrcreen(props){
+	useEffect(function(){
+		fetch("https://clockwiseserver.herokuapp.com/get_masters")
+		.then(data=>data.json())
+		.then(data=>props.initMasters(data))
+	}, []);
 	function addNewMasterHandler(e){
 			e.preventDefault();
 			let masterName = e.target.name.value;
@@ -18,12 +23,22 @@ function AdminSrcreen(props){
 			let townsArr = selectCheckedTowns(e.target.elements)
 			if(masterName&&masterRating&&townsArr.length!==0){
 				let infoObj = {
+					id: createUniqueId(),
 					name: masterName,
 					rating: masterRating,
-					id: createUniqueId(),
-					towns: townsArr
+					towns: townsArr.join(",")
 				};
-				props.addNewMaster(infoObj);
+				let strObj = JSON.stringify(infoObj);
+				fetch("https://clockwiseserver.herokuapp.com/post_master", {
+					method: 'POST',
+					headers: {
+						'Content-Type': 'application/json;charset=utf-8'
+					},
+					body: strObj,
+				})
+				.then(json=>json.json())
+				.then(data=>props.addNewMaster(data));
+
 				alert("You added new master");
 				props.history.push('/admin/mastersList');
 			}else{
@@ -98,5 +113,6 @@ function mapStateToProps(state){
 let actions = {
 	addNewMaster,
 	addNewTown,
+	initMasters
 }
 export default connect(mapStateToProps, actions)(AdminSrcreen);
