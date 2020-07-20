81b8952eda6352c7c31134129b0737bdbd781d70
EvgenySverchkov <47426436+EvgenySverchkov@users.noreply.github.com>
2020-07-20 10:51:06 +0300

    little change

--- src/components/AdminScreen/services/postData.js
+++ src/components/AdminScreen/services/postData.js
@@ -2,7 +2,7 @@ export default (url, newObj) =>
   fetch(url, {
     method: "POST",
     headers: {
-      Authorization: "Bearer " + sessionStorage.getItem("token") || "",
+      Authorization: sessionStorage.getItem("token")? "Bearer" + sessionStorage.getItem("token") : "",
       "Content-Type": "application/json",
     },
     body: JSON.stringify(newObj),
